2cad1ee778feff8071b6ab0ad08a9fd6c04e11ad
Mateus Berger <mateusberger96@gmail.com>
2022-09-09 18:28:57 -0300

    Feat: Commit inicial, criado projeto Spring da api

---
+++ sessoesdevoto/src/main/java/com/desafio/sessoesdevoto/SessoesdevotoApplication.java
@@ -0,0 +1,13 @@
+package com.desafio.sessoesdevoto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SessoesdevotoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SessoesdevotoApplication.class, args);
+	}
+
+}
---
+++ sessoesdevoto/src/test/java/com/desafio/sessoesdevoto/SessoesdevotoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafio.sessoesdevoto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SessoesdevotoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
