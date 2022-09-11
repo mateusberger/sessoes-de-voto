bba4f9dcd2fa9fe0981f16a79d40c1c80e40a814
Mateus Berger <mateusberger96@gmail.com>
2022-09-11 18:50:29 -0300

    Feat: Adicionado alguns testes de unidade para a classe PautaServiceImple

--- sessoesdevoto/src/test/java/com/desafio/sessoesdevoto/SessoesdevotoApplicationTests.java
+++ sessoesdevoto/src/test/java/com/desafio/sessoesdevoto/SessoesdevotoApplicationTests.java
@@ -1,13 +1,12 @@
 package com.desafio.sessoesdevoto;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SessoesdevotoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
