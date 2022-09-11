0a8e8d30f05a687c2c8b5525b92083b2c4116b7c
Mateus Berger <mateusberger96@gmail.com>
2022-09-11 18:38:17 -0300

    Feat: Adicionado log de debug nas chamadas ao repositório

--- sessoesdevoto/src/main/java/com/desafio/sessoesdevoto/infraestrutura/repositorios/nosql/PautaRepoImpleMongo.java
+++ sessoesdevoto/src/main/java/com/desafio/sessoesdevoto/infraestrutura/repositorios/nosql/PautaRepoImpleMongo.java
@@ -4,6 +4,8 @@ import com.desafio.sessoesdevoto.dominio.Pauta;
 import com.desafio.sessoesdevoto.dominio.excecoes.PautaNaoEncotradaException;
 import com.desafio.sessoesdevoto.dominio.portas.repositorios.PautaRepositoryPort;
 import com.desafio.sessoesdevoto.infraestrutura.entidades.PautaEntity;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
@@ -13,27 +15,37 @@ import java.util.Optional;
 @Repository
 public class PautaRepoImpleMongo implements PautaRepositoryPort {
 
+    private static final Logger logger = LoggerFactory.getLogger(PautaRepoImpleMongo.class);
+
     @Autowired
     private PautaMongoRepository pautaRepo;
 
     @Override
     public Pauta registrarPauta(Pauta pauta) {
+
+        logger.debug("Iniciado registro de pauta. ", pauta);
+
         return pautaRepo.save(new PautaEntity(pauta)).toPauta();
     }
 
     @Override
     public Optional<Pauta> buscarPeloId(String idPauta) {
+
+        logger.debug("Iniciado busca de pauta ID: " + idPauta);
+
         return pautaRepo.findById(idPauta).map(PautaEntity::toPauta);
     }
 
     @Override
     public Pauta alterarPauta(Pauta pauta) {
+
+        logger.debug("Iniciado alteração de pauta: ", pauta);
+
         Optional<PautaEntity> pautaOptional = pautaRepo.findById(pauta.getId());
 
         if (pautaOptional.isEmpty()) throw new PautaNaoEncotradaException();
 
         PautaEntity pautaEntity = pautaOptional.get();
-
         pautaEntity.atualizar(pauta);
 
         return pautaRepo.save(pautaEntity).toPauta();
@@ -41,6 +53,9 @@ public class PautaRepoImpleMongo implements PautaRepositoryPort {
 
     @Override
     public List<Pauta> listarTodas() {
+
+        logger.debug("Iniciando listagem de pautas");
+
         return pautaRepo.findAll().stream().map(PautaEntity::toPauta).toList();
     }
 }
--- sessoesdevoto/src/main/java/com/desafio/sessoesdevoto/infraestrutura/repositorios/nosql/VotoRepoImpleMongo.java
+++ sessoesdevoto/src/main/java/com/desafio/sessoesdevoto/infraestrutura/repositorios/nosql/VotoRepoImpleMongo.java
@@ -4,6 +4,8 @@ import com.desafio.sessoesdevoto.dominio.Voto;
 import com.desafio.sessoesdevoto.dominio.excecoes.PautaNaoEncotradaException;
 import com.desafio.sessoesdevoto.dominio.portas.repositorios.VotoRepositoryPort;
 import com.desafio.sessoesdevoto.infraestrutura.entidades.PautaEntity;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.ArrayList;
@@ -12,16 +14,22 @@ import java.util.Optional;
 
 public class VotoRepoImpleMongo implements VotoRepositoryPort {
 
+    private static final Logger logger = LoggerFactory.getLogger(VotoRepoImpleMongo.class);
     @Autowired
     private PautaMongoRepository pautaRepo;
 
     @Override
     public boolean existeVoto(String cpfDoAssociado, String idPauta) {
+
+        logger.debug("Iniciado verificação de existencia de voto com cpf: " + cpfDoAssociado + " e pauta ID: " + idPauta);
+
         return pautaRepo.existsByIdAndVotosCpfDoAssociado(idPauta, cpfDoAssociado);
     }
 
     @Override
     public Voto registrarVoto(Voto voto) {
+
+        logger.debug("Iniciado registro de voto.", voto);
 
         Optional<PautaEntity> pautaEntityOptional = pautaRepo.findById(voto.getIdDaPauta());
 
@@ -38,6 +46,9 @@ public class VotoRepoImpleMongo implements VotoRepositoryPort {
 
     @Override
     public List<Voto> listarPeloIdDaPauta(String idPauta) {
+
+        logger.debug("Iniciado listagem de votos da pauta ID: " + idPauta);
+
         Optional<PautaEntity> pautaOptional = pautaRepo.findById(idPauta);
 
         if (pautaOptional.isEmpty()) return new ArrayList<>();
