d8e6bceda3f9e5b843f5e5b78d36172a5fd462bd
Mateus Berger <mateusberger96@gmail.com>
2022-09-11 18:42:31 -0300

    Doc: Aprimorada a documentação da API

--- sessoesdevoto/src/main/java/com/desafio/sessoesdevoto/aplicacao/VotoController.java
+++ sessoesdevoto/src/main/java/com/desafio/sessoesdevoto/aplicacao/VotoController.java
@@ -1,11 +1,24 @@
 package com.desafio.sessoesdevoto.aplicacao;
 
-import com.desafio.sessoesdevoto.dominio.dto.RegistrarVotoForm;
+import com.desafio.sessoesdevoto.aplicacao.exceptionhandler.MensagemDeErroPadrao;
+import com.desafio.sessoesdevoto.dominio.dto.VotoCompletoDTO;
+import com.desafio.sessoesdevoto.dominio.form.RegistrarVotoForm;
 import com.desafio.sessoesdevoto.dominio.portas.interfaces.VotoService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+//<editor-fold defaultstate="collapsed" desc="Swagger Doc">
+@Tag(
+        name = "Voto",
+        description = "Controladora para registro de votos"
+)
+//</editor-fold>
 @RestController
 @RequestMapping("/voto")
 public class VotoController {
@@ -13,11 +26,71 @@ public class VotoController {
     @Autowired
     private VotoService votoService;
 
+    //<editor-fold defaultstate="collapsed" desc="Doc">
+    @Operation(
+            summary = "Registrar um voto",
+            description =
+                    "Registra um voto utilizando o CPF do associado e o ID da pauta.\n\n" +
+                    "O atributo voto contem a resposta do voto (true = SIM, false = NÃO).",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "201",
+                            description = "Voto registrado com sucesso",
+                            content = {
+                                    @Content(
+                                            mediaType = "application/json",
+                                            schema = @Schema(implementation = VotoCompletoDTO.class)
+                                    )
+                            }
+                    ),
+                    @ApiResponse(
+                            responseCode = "400",
+                            description = "Atributos ou valores incorretos",
+                            content = {
+                                    @Content(
+                                            mediaType = "application/json",
+                                            schema = @Schema(implementation = MensagemDeErroPadrao.class)
+                                    )
+                            }
+                    ),
+                    @ApiResponse(
+                            responseCode = "403",
+                            description = "Sessão de votação encerrada ou ainda não iniciada",
+                            content = {
+                                    @Content(
+                                            mediaType = "application/json",
+                                            schema = @Schema(implementation = MensagemDeErroPadrao.class)
+                                    )
+                            }
+                    ),
+                    @ApiResponse(
+                            responseCode = "409",
+                            description = "Voto já computado",
+                            content = {
+                                    @Content(
+                                            mediaType = "application/json",
+                                            schema = @Schema(implementation = MensagemDeErroPadrao.class)
+                                    )
+                            }
+                    ),
+                    @ApiResponse(
+                            responseCode = "500",
+                            description = "Erro interno no servidor",
+                            content = {
+                                    @Content(
+                                            mediaType = "application/json",
+                                            schema = @Schema(implementation = MensagemDeErroPadrao.class)
+                                    )
+                            }
+                    )
+            }
+    )
+    //</editor-fold>
     @PostMapping
-    public ResponseEntity registrarVoto(
+    @ResponseStatus(HttpStatus.CREATED)
+    public VotoCompletoDTO registrarVoto(
             @RequestBody RegistrarVotoForm registrarVotoForm
     ) {
-
-        return ResponseEntity.ok(votoService.registrarVoto(registrarVotoForm));
+        return votoService.registrarVoto(registrarVotoForm);
     }
 }
